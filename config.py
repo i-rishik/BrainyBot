d4e9b5fbfcf04cf802c9f4cc390b17235d0b672a
kalyan4270 <107978401+kalyan4270@users.noreply.github.com>
2025-02-04 14:25:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
 # Set API key for ChatGroq
-os.environ["GROQ_API_KEY"] = "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"
+os.environ["GROQ_API_KEY"] = "Paste_Created_API_Key"
