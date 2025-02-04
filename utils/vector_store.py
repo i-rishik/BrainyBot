602550f5972fa36c11baac0c1959a94ce5d2b81d
Ch Yaswanth <107978401+kalyan4270@users.noreply.github.com>
2025-02-04 19:43:42 +0530

    Update vector_store.py

--- utils/vector_store.py
+++ utils/vector_store.py
@@ -5,5 +5,5 @@ from utils.embeddings import create_embeddings
 
 def create_vector_store(texts):
     embeddings = create_embeddings()
-    persist_directory = ".\vector_db"
+    persist_directory = "vector_db"
     return Chroma.from_documents(texts, embeddings, persist_directory=persist_directory)
